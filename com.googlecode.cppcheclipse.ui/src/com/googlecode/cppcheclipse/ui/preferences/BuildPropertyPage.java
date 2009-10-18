598d3a2b405cb9441ea7f1930ab495beeed8c7d7
konrad_w <konrad_w@gmx.de>
2009-10-18 02:38:41 +0000

    initial import of UI plugin

---
+++ com.googlecode.cppcheclipse.ui/src/com/googlecode/cppcheclipse/ui/preferences/BuildPropertyPage.java
@@ -0,0 +1,69 @@
+package com.googlecode.cppcheclipse.ui.preferences;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.jface.preference.BooleanFieldEditor;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.ui.IWorkbenchPropertyPage;
+
+import com.googlecode.cppcheclipse.core.CppcheclipsePlugin;
+import com.googlecode.cppcheclipse.core.Nature;
+import com.googlecode.cppcheclipse.core.PreferenceConstants;
+
+public class BuildPropertyPage extends FieldEditorPreferencePage implements
+		IWorkbenchPropertyPage {
+	private IAdaptable element;
+
+	private BooleanFieldEditor runOnBuild;
+	
+	public BuildPropertyPage() {
+		// must set preference store later
+		runOnBuild = null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.jface.preference.FieldEditorPreferencePage#createFieldEditors
+	 * ()
+	 */
+	@Override
+	protected void createFieldEditors() {
+		runOnBuild = new BooleanFieldEditor(PreferenceConstants.P_RUN_ON_BUILD,
+				"Run on build", getFieldEditorParent());
+		addField(runOnBuild);
+	}
+
+	@Override
+	public boolean performOk() {
+		boolean result = super.performOk();
+		if (result) {
+			if (runOnBuild != null) {
+				Nature.toggleNature((IProject)getElement(), runOnBuild.getBooleanValue());
+			}
+		}
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.IWorkbenchPropertyPage#getElement()
+	 */
+	public IAdaptable getElement() {
+		return element;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.IWorkbenchPropertyPage#setElement(org.eclipse.core.runtime
+	 * .IAdaptable)
+	 */
+	public void setElement(IAdaptable element) {
+		this.element = element;
+		setPreferenceStore(CppcheclipsePlugin.getProjectPreferenceStore((IProject) getElement(), false));
+	}
+}
