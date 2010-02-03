3f0ea716fa62202fcbbf07d36bcea769e8bdbc05
konrad_w@gmx.de <konrad_w@gmx.de@13587959-879a-e0fe-f2c8-ed69f43d7837>
2010-02-03 20:05:04 +0000

    use commands instead of actions, add key binding

--- trunk/com.googlecode.cppcheclipse.ui/src/com/googlecode/cppcheclipse/ui/actions/RunCodeAnalysis.java
+++ trunk/com.googlecode.cppcheclipse.ui/src/com/googlecode/cppcheclipse/ui/actions/RunCodeAnalysis.java
@@ -1,45 +1,88 @@
 package com.googlecode.cppcheclipse.ui.actions;
 
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.QualifiedName;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.SubProgressMonitor;
 import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.ui.IObjectActionDelegate;
-import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IFileEditorInput;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 import com.googlecode.cppcheclipse.core.CppcheclipsePlugin;
 import com.googlecode.cppcheclipse.ui.Messages;
 
-public class RunCodeAnalysis implements IObjectActionDelegate {
-	private ISelection sel;
+public class RunCodeAnalysis extends AbstractHandler {
+	private static final QualifiedName SELECTION_PROPERTY = new QualifiedName("com.googlecode.cppcheclipse", "JobSelection");
 
-	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+	private IStructuredSelection getEditorSelection(IEditorPart editor) {
+		if (editor == null) {
+			return null;
+		}
+		IEditorInput input = editor.getEditorInput();
+		if (input == null) {
+			return null;
+		}
+		
+		IFileEditorInput fileInput = (IFileEditorInput) input.getAdapter(IFileEditorInput.class);
+		if (fileInput == null) {
+			return null;
+		}
+		List<IFile> fileList = new LinkedList<IFile>();
+		fileList.add(fileInput.getFile());
+		return new StructuredSelection(fileList);
 	}
-
-	public void run(IAction action) {
+	
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// get selection
+		ISelection selection = HandlerUtil.getCurrentSelection(event); 
+		if (!(selection instanceof IStructuredSelection)) {
+			if (selection instanceof ITextSelection) {
+				selection = getEditorSelection(HandlerUtil.getActiveEditor(event));
+				if (selection == null) {
+					return null;
+				}
+			}
+		}
+		
 		Job job = new Job(Messages.RunCodeAnalysis_JobName) {
 			@SuppressWarnings("unchecked")
 			@Override
 			protected IStatus run(final IProgressMonitor monitor) {
-				IStructuredSelection ss = (IStructuredSelection) sel;
+				IStructuredSelection ss = (IStructuredSelection)getProperty(SELECTION_PROPERTY);
+			
 				int count = ss.size() * 100;
 				monitor.beginTask(getName(), count);
 				if (monitor.isCanceled())
 					return Status.CANCEL_STATUS;
 				Builder builder = new Builder();
 				for (Iterator iterator = ss.iterator(); iterator.hasNext();) {
-					Object o = iterator.next();
-					if (o instanceof IResource) {
-						IResource res = (IResource) o;
-						SubProgressMonitor subMon = new SubProgressMonitor(
+					Object elem = iterator.next();
+					// Adapt the first element to a file.
+					 if (!(elem instanceof IAdaptable))
+					      continue;
+					   IResource res = (IResource) ((IAdaptable) elem).getAdapter(IResource.class);
+					   if (res == null)
+					      continue;
+					   
+					SubProgressMonitor subMon = new SubProgressMonitor(
 								monitor, 100);
 						try {
 							builder.processResource(res, subMon);
@@ -47,19 +90,18 @@ public class RunCodeAnalysis implements IObjectActionDelegate {
 							CppcheclipsePlugin.showError(Messages.bind(Messages.RunCodeAnalysis_Error, res.getName()), e1);
 						}
 						subMon.done();
-					}
+					
 					if (monitor.isCanceled())
 						return Status.CANCEL_STATUS;
 				}
 				return Status.OK_STATUS;
 			}
 		};
+		job.setProperty(SELECTION_PROPERTY, (IStructuredSelection)selection);
 		job.setUser(true);
 		job.schedule();
-	}
 
-	public void selectionChanged(IAction action, ISelection selection) {
-		this.sel = selection;
+		return null;
 	}
 }
 
