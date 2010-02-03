962b4193eae288b3d4942ead760a269d7ff83efa
konrad_w@gmx.de <konrad_w@gmx.de@13587959-879a-e0fe-f2c8-ed69f43d7837>
2010-02-03 20:07:08 +0000

    fix error message

--- trunk/com.googlecode.cppcheclipse.core/src/com/googlecode/cppcheclipse/core/command/IncompatibleVersionException.java
+++ trunk/com.googlecode.cppcheclipse.core/src/com/googlecode/cppcheclipse/core/command/IncompatibleVersionException.java
@@ -8,7 +8,7 @@ public class IncompatibleVersionException extends ProcessExecutionException {
 	private static final long serialVersionUID = 6032181437996210576L;
 
 	public IncompatibleVersionException(Version version) {
-		super("You must have at least version " + Version.MIN_VERSION.toString() + " of cppcheck. But you only have version "+ version.toString());
+		super("You must have at least version " + Version.MIN_VERSION.toString() + " of cppcheck. But you only have version "+ version.toString() +"!");
 	}
 
 }
