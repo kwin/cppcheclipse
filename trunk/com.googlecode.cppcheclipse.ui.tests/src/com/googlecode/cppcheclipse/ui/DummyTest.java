744e7acf05c3ea285276a4c4cb0fb05a50f58c17
konrad_w@gmx.de <konrad_w@gmx.de@13587959-879a-e0fe-f2c8-ed69f43d7837>
2011-11-27 10:57:05 +0000

    add tycho project for ui tests

---
+++ trunk/com.googlecode.cppcheclipse.ui.tests/src/com/googlecode/cppcheclipse/ui/DummyTest.java
@@ -0,0 +1,13 @@
+package com.googlecode.cppcheclipse.ui;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+	@Test
+	public void testDummy() {
+		assertTrue(true);
+	}
+}
