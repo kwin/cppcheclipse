3ebe0c0deb557797d474e7768e855e27e1b93976
konrad_w <konrad_w@gmx.de>
2014-03-01 20:17:33 +0000

    fix some warnings

--- com.googlecode.cppcheclipse.core/src/com/googlecode/cppcheclipse/core/command/AbstractCppcheckCommand.java
+++ com.googlecode.cppcheclipse.core/src/com/googlecode/cppcheclipse/core/command/AbstractCppcheckCommand.java
@@ -6,7 +6,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.StringReader;
 import java.io.UnsupportedEncodingException;
 import java.text.SimpleDateFormat;
